8a8ffbde2bb07246dfd665d0400bb79ec73b3ab5
Greg Allen <me@jga.me>
2012-09-23 14:25:29 -0700

    updated tests

--- test/generate.test.js
+++ test/generate.test.js
@@ -4,19 +4,25 @@ var path = require('path');
 var fs = require('fs');
 var exists = fs.existsSync || path.existsSync;
 var combinations = require('../lib/combinations');
+var data = require('../data.json');
 
 
 suite('generate', function() {
   test('make sure all files have been generated', function() {
 
     var combos = combinations();
+    var versions = data.versions
 
-    combos.forEach(function(combo) {
-      var f1 = filename(combo, false);
-      assert.equal(exists(path.join(__dirname, '../dist/'+f1)), true, f1)
-      var f2 = filename(combo, true);
-      assert.equal(exists(path.join(__dirname, '../dist/'+f2)), true, f2)
-    });
+
+    versions.forEach(function(version) { 
+
+      combos.forEach(function(combo) {
+        var f1 = filename(combo, false);
+        assert.equal(exists(path.join(__dirname, '../dist/'+version+'/'+f1)), true, f1)
+        var f2 = filename(combo, true);
+        assert.equal(exists(path.join(__dirname, '../dist/'+version+'/'+f2)), true, f2)
+      });
+    })
     
   });
 });
