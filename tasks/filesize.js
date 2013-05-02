6cd7dd8e68d8e53c7db6efe5c78be212939ae496
Greg Allen <me@jga.me>
2013-05-01 18:16:36 -0700

    added filesize task to get filesizes for dist files

---
+++ tasks/filesize.js
@@ -0,0 +1,74 @@
+var grunt = require('grunt');
+var bytesize = require('bytesize');
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+
+
+var baseDir = path.join(__dirname, '../dist');
+
+var getFileSize = function(file, callback) {
+  bytesize.fileSize(file, false, function(err, size) {
+    if (err) {
+      return callback(err);
+    }
+    bytesize.gzipSize(file, false, function(err, gzipsize) {
+      callback(err, {
+        file: path.basename(file),
+        normal: size,
+        gzip: gzipsize
+      });
+    });
+  });
+}
+
+var readVersion = function(version, callback) {
+  var dir = path.join(baseDir, version);
+  fs.readdir(dir, function(err, files) {
+    files = files.map(function(file) {
+      return path.join(dir, file);
+    });
+    async.mapSeries(files, getFileSize, function(err, results) {
+      var out = {};
+      results.forEach(function(result) {
+        out[result.file] = { normal: result.normal, gzip: result.gzip };
+      });
+      callback(err, { version: version, files: out });
+    });
+  });
+}
+
+var writeFile = function(obj, callback) {
+  var jsonString = JSON.stringify(obj);
+  fs.writeFile(path.join(__dirname, '../filesizes.json'), jsonString, callback);
+}
+
+var run = function(callback) {
+  fs.readdir(baseDir, function(err, files) {
+    var versions = [];
+    files.forEach(function(file) {
+      var stat = fs.statSync(path.join(baseDir, file));
+      if (stat.isDirectory()) {
+        versions.push(file);
+      }
+    });
+
+    async.mapSeries(versions, readVersion, function(err, results) {
+      var sizes = {};
+      results.forEach(function(obj) {
+        sizes[obj.version] = obj.files;
+      });
+      writeFile(sizes, callback);
+    });
+  });
+}
+
+grunt.registerTask('filesize', function() {
+  var done = this.async();
+
+  run(done);
+
+});
+run(function() {});
+
+
