561d58c97a8beeef0d72bc3e98a0531762eac3b1
Peter Kinmond <peterkinmond@gmail.com>
2014-09-24 10:54:56 -0700

    Added 1.11.0

--- bin/builder.js
+++ bin/builder.js
@@ -27,7 +27,7 @@ var opt = require('optimist')
       alias: 'version',
       describe: 'Version of jQuery',
       type: 'string',
-      default: '1.10.2'
+      default: '1.11.0'
     })
     .options('s', {
       alias: 'versions',
